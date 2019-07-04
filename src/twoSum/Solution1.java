e32dcf3012ddbf783b766e36bc965feb3ec3dc6c
Michael199404 <1183787376@qq.com>
2019-07-04 10:47:09 +0800

    两数之和

---
+++ src/twoSum/Solution1.java
@@ -0,0 +1,29 @@
+package twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+* 两数之和
+* 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那
+* 两个整数，并返回他们的数组下标。
+*
+* 思路：
+*
+* */
+public class Solution1 {
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i){
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        return null;
+    }
+}
